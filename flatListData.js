73dcd40192ea37580be3d1142ceaa9c090d66b66
Izza Syazira <49303572+izzasyazira@users.noreply.github.com>
2019-04-19 17:19:37 +0800

    Learn flatListData , FlatList

---
+++ flatListData.js
@@ -0,0 +1,88 @@
+
+
+var flastListData = [
+
+    {
+        "key": "1",
+        "name":"innisfree1",
+        "foodDescription":"This is for toner"
+    },
+
+    {
+        "key": "2",
+        "name":"innisfree2",
+        "foodDescription":"This is for toner"
+
+    },
+
+    {
+        "key": "3",
+        "name":"innisfree3",
+        "foodDescription":"This is for toner"
+
+    },
+
+    {
+        "key": "4",
+        "name":"innisfree4",
+        "foodDescription":"This is for toner"
+
+    },
+
+    {
+        "key": "5",
+        "name":"innisfree5",
+        "foodDescription":"This is for toner"
+
+    },
+
+    {
+        "key": "6",
+        "name":"innisfree6",
+        "foodDescription":"This is for toner"
+
+    },
+
+
+
+];
+export default flatListData;
+
+// class FlatListItem extends React.Component {
+  
+//   render() {
+//     return (
+//         <View style ={{
+//             flex:1,
+//             backgroundColor:this.props.index % 2==0 ? 'mediumseagreen' : 'tomato'
+//         }} >
+//           <Text>{this.props.item.name}</Text>
+//           <Text>{this.props.item.foodDescription}</Text>
+//         </View>
+
+//     );
+  
+// }}
+
+// export default class BasicFlatList extends React.Component {
+  
+//   render() {
+//     return (
+//         <View style ={{flex:1,marginTop:22}} >
+//             <FlatList
+//                 data={flatListData}
+//                 renderItem={({item,index})=> {
+//                   return(<FlatListItem item = {item} index = {index}>
+                  
+                  
+//                   </FlatListItem>);
+//                 }}
+//                 >
+
+//             </FlatList>
+//         </View>
+
+//     );
+  
+// }}
+
